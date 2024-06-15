1591e49b98ddcfd42e63ab697b2ea3cb06bc4f7f
Divy Gupta <100290201+Divygupta208@users.noreply.github.com>
2024-06-16 01:40:37 +0530

    Time: 8 ms (9.98%), Space: 12.6 MB (51.07%) - LeetHub

---
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+        
+        if(k>1){
+            sort(s.begin() , s.end());
+            return s ;
+        }
+        
+        string result = s;
+        for(int l = 1 ; l<=s.length() ; l++){
+            string temp = s.substr(l) + s.substr(0,l);
+            result = min(result , temp);
+        }
+        
+        return result ;
+    }
+};
